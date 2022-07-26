3354c09076d8c1854e236d6fbd049c3df750234b
Deenadhayalan17 <deena17698ddd@gmail.com>
2022-07-26 21:16:15 +0530

    Reverse a String in C# without using Built-in metho

---
+++ HelloWorld/Logical Problems/ReverseString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld.Logical_Problems
+{
+    public class ReverseString {
+
+        //Reverse a String in C# without using Built-in metho
+        public void Method1()
+        {
+            Console.WriteLine("Enter the string : ");
+            string input = Console.ReadLine();
+            string output = string.Empty;
+
+            for(int i = input.Length -1 ; i>=0;i--)
+            {
+                output += input[i];
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
