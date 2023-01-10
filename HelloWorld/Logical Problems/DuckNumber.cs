9c87933e1e05a53ac602dd245d55569cf1bb37e3
DEENADAYALAN D <DEENADAYALAN D@DESKTOP-FN86O3S>
2023-01-10 19:37:50 +0530

    find all the duck numbers between 1 to 100

--- HelloWorld/Logical Problems/DuckNumber.cs
+++ HelloWorld/Logical Problems/DuckNumber.cs
@@ -3,43 +3,63 @@
 namespace HelloWorld.Logical_Problems
 {
 
-        public class DuckNumber
+    public class DuckNumber
+    {
+
+        public void Method1()
         {
+            Console.WriteLine("enter a number to check :");
 
-            public void Method1()
+            int number = Convert.ToInt32(Console.ReadLine());
+            string str = number.ToString();
+
+            int l = str.Length;
+
+            int count = 0;
+
+            for (int i = 1; i < l; i++)
             {
-                Console.WriteLine("enter a number to check :");
-
-                int number = Convert.ToInt32(Console.ReadLine());
-                string str = number.ToString();
-
-                int l = str.Length;
-
-                int count = 0;
-
-                for (int i =1; i < l; i++)
+                char c = str[i];
+                if (c == '0')
                 {
-                    char c = str[i];
-                    if (c == '0')
-                    {
-                        count++;
-                        break;
-                    }
+                    count++;
+                    break;
                 }
-                char f = str[0];
+            }
+            char f = str[0];
 
-                if(count>0 &&(f=='0'|| f != '0'))
-                {
-                    Console.WriteLine("yes its a duck number");
-                }
-                else
-                {
-                    Console.WriteLine("its not a duck number");
-                }
-
+            if (count > 0 && (f == '0' || f != '0'))
+            {
+                Console.WriteLine("yes its a duck number");
+            }
+            else
+            {
+                Console.WriteLine("its not a duck number");
             }
 
+        }
+        //find all the duck numbers between 1 to 100
+        public void Method2()
+        {
+            Console.WriteLine("the duck number below 100 are :");
 
-        
+            for (int i=0; i <= 100; i++) {
+                int current = i;
+                int count=0;
+                while(current > 0)
+                {
+                    if (current % 10 == 0)
+                    {
+                        count = 1;
+                        break;
+                    }
+                    current = current / 10;
+                }
+                if(i>0 && count == 1)
+                {
+                    Console.WriteLine(" " + i);
+                }
+            }
+        }
     }
 }
